459d985e693bcd2afb0b46a2da4a397ed4fd33c9
Eissa Faheem <eissa.faheem@ziroh.com>
2023-05-03 12:47:46 +0530

    updated

--- ButtonComponent/button.component.tsx
+++ ButtonComponent/button.component.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, TouchableHighlight } from 'react-native';
 import { ButtonComponentStyles } from './button.styles';
 
-export type ButtonType = "PRIMARY" | "SECONDARY";
+export type ButtonType = "FILLED" | "OUTLINED";
 
 type ButtonComponentProps = {
-  buttonText: string,
   buttonType: ButtonType,
+  buttonText: string,
+  buttonTextColor: string,
+  buttonBackgroundColor: string,
+  buttonUnderlayColor: string,
   onPress: () => void
 }
 
-export const ButtonComponent = (props: ButtonComponentProps) => (
-  <TouchableOpacity onPress={props.onPress} style={ButtonComponentStyles[props.buttonType]}>
-    <Text style={
-      props.buttonType === "PRIMARY"
-        ? ButtonComponentStyles.primaryButtonText
-        : ButtonComponentStyles.secondaryButtonText
-    }>{props.buttonText}</Text>
-  </TouchableOpacity>
-);
+export const ButtonComponent = (props: ButtonComponentProps) => {
+
+  let backgroundColor: string = "";
+  let borderColor: string = "";
+  let textColor: string = "";
+
+  switch (props.buttonType) {
+    case 'FILLED':
+      backgroundColor = props.buttonBackgroundColor;
+      borderColor = props.buttonBackgroundColor;
+      textColor = props.buttonTextColor;
+      break;
+    case 'OUTLINED':
+      backgroundColor = "white";
+      borderColor = "#79747e";
+      textColor = props.buttonBackgroundColor;
+      break;
+  }
+
+  return (
+    <TouchableHighlight underlayColor={props.buttonUnderlayColor} onPress={props.onPress} style={[
+      // ButtonComponentStyles[props.buttonType],
+      ButtonComponentStyles.buttonCommonStyles,
+      {
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+      }
+    ]}>
+      <Text style={
+        [
+          ButtonComponentStyles.buttonText,
+          {
+            color: textColor
+          }
+        ]
+      }>{props.buttonText}</Text>
+    </TouchableHighlight>
+  );
+}
--- ButtonComponent/button.styles.ts
+++ ButtonComponent/button.styles.ts
@@ -2,39 +2,19 @@ import { StyleSheet } from "react-native";
 
 export const ButtonComponentStyles = StyleSheet.create({
 
-    primaryButtonText: {
-        color: 'white',
+    buttonCommonStyles:{
+        alignSelf: "center",
+        paddingHorizontal: 24,
+        paddingVertical: 10,
+        borderRadius: 100,
+        borderWidth: 2,
+    },
+
+    buttonText: {
         textAlign: 'center',
-        fontWeight: "500",
+        fontWeight: "700",
         fontFamily: "Poppins",
-        fontSize: 15,
-    },
-
-    secondaryButtonText: {
-        color: '#333333',
-        textAlign: 'center',
-        fontWeight: "500",
-        fontFamily: "Poppins",
-        fontSize: 15,
-    },
-
-    PRIMARY: {
-        backgroundColor: "#40B876",
-        borderColor: "#40B876",
-        border: 1,
-        borderWidth: 1,
-        paddingHorizontal: 20,
-        paddingVertical: 5,
-        borderRadius: 7
-    },
-
-    SECONDARY: {
-        backgroundColor: "white",
-        borderColor: "#CBD5E1",
-        border: 1,
-        borderWidth: 1,
-        paddingHorizontal: 20,
-        paddingVertical: 5,
-        borderRadius: 7
+        fontSize: 14,
+        letterSpacing:1.2
     },
 })
