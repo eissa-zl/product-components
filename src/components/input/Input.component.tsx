d353e61d3f829893ecfd1d5989d0bbc9f28f173d
SuyashSrivastavaZiroh <suyash.srivastava@ziroh.com>
2023-05-12 12:35:44 +0530

    feat: added input component

---
+++ src/components/input/Input.component.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { View, Text, TextInput,KeyboardAvoidingView, StyleProp, StyleSheet } from 'react-native'
+import styles from '../input/Input.styles'
+
+export const TextInputComponent = ({label, value, onChange, placeholder = "Type Here",supportingText="", placeholderTextColor = "grey", secureTextEntry = false, showSupportingText = false, onBlur, onFocus, inputstyle,labelstyle,supportingtextstyle,}: TextInputComponentProps) => {
+    const { containerStyles, labelStyles, supportingTextStyles, inputStyles } = styles
+
+    //Label Background is is White
+
+    return (
+        <KeyboardAvoidingView behavior="padding" style={containerStyles}>
+            <Text style={{...labelStyles,...labelstyle}}>{label}</Text> 
+            <View>
+                <TextInput
+                    style={{...inputStyles,...inputstyle}}
+                    placeholder={placeholder}
+                    value={value}
+                    onChangeText={onChange}
+                    autoCorrect={false}
+                    multiline={true}
+                    secureTextEntry={secureTextEntry}
+                    placeholderTextColor={placeholderTextColor}
+                    textAlignVertical={'center'}
+                    onBlur={onBlur}
+                />
+            </View>
+            {showSupportingText && <Text style={{...supportingTextStyles,...supportingtextstyle}}>{supportingText}</Text>}
+        </KeyboardAvoidingView >
+    )
+}
+
+type TextInputComponentProps = {
+    label: string,
+    value: string,
+    showSupportingText: boolean,
+    placeholder: string,
+    onChange: () => void,
+    supportingText?:string,
+    inputstyle?:any,
+    labelstyle?:any,
+    supportingtextstyle?:any,
+    secureTextEntry?: boolean,
+    placeholderTextColor?: string,
+    onFocus?: () => void,
+    onBlur?: () => void,
+}
