03ab710efd867e6a68b709bfc1db48af2b02cc2f
SuyashSrivastavaZiroh <suyash.srivastava@ziroh.com>
2023-05-12 12:41:38 +0530

    exported component

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { ButtonComponent } from "./src/components/button/Button.component";
 import { BadgeComponent } from "./src/components/badge/Badge.component";
+import {TextInputComponent} from './src/components/input/Input.component'
 
 export {
     ButtonComponent,
-    BadgeComponent
+    BadgeComponent,
+    TextInputComponent
 };
