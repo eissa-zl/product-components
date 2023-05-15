77ab7c0601c5e0fd5c7ba003b462c6f0c0aeeb04
rizwan-zl <rizwan.zkazi@ziroh.com>
2023-05-15 10:48:52 +0530

    exported component

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { ButtonComponent } from "./src/components/button/Button.component";
 import { BadgeComponent } from "./src/components/badge/Badge.component";
-import {TextInputComponent} from './src/components/input/Input.component'
+import { TextInputComponent } from './src/components/input/Input.component'
+import { RadioComponent } from "./src/components/radioButton/Radio.component";
 
 export {
     ButtonComponent,
     BadgeComponent,
-    TextInputComponent
+    TextInputComponent,
+    RadioComponent,
 };
